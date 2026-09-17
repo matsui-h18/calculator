d7ef26d8c5b7baf67d2f6172267efafe33ed1fe6
Honoka <matsui18@example.com>
2026-09-18 01:08:18 +0900

    add plus and minus

--- src/main/java/com/github/matsui_h18/portfolio/controller/MainController.java
+++ src/main/java/com/github/matsui_h18/portfolio/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -15,11 +16,40 @@ public class MainController {
 		return "main";
 	}
 	
-	// 「0」が押されたとき
-	@PostMapping("/")
-	public String num0(Model model) {
+	// 数字が押されたとき
+	@PostMapping(value = "/", params="btn")
+	public String numPress(@RequestParam("btn") int num, Model model) {
 		// 画面の数字が出力されるところに数字を返す
-		model.addAttribute("display", "0");
+		model.addAttribute("display", num);
 		return "main";
 	}
+	
+	// 符号が押されたとき
+	@PostMapping(value = "/", params="sign")
+	public String signPress(@RequestParam("sign") String sign, Model model) {
+		// 画面の数字が出力されるところに数字を返す
+		if("+".equals(sign)) {
+			model.addAttribute("display", sign);
+		}else if("-".equals(sign)) {
+			model.addAttribute("display", sign);
+		}
+		
+		return "main";
+	}
+	
+	// 計算用
+	/*
+	public String calc(int num1, int num2, String sign) {
+		int result;
+		if("+".equals(sign)) {
+			result = (num1 + num2);
+			model.addAttribute("display", result);
+		}else if("-".equals(sign)) {
+			result = (num1 - num2);
+			model.addAttribute("display", result);
+		}
+				
+		return "main";
+	}
+	*/
 }
